37c69a7adfa23bd314747c06dae788f0128422ec
VitorBiliato10 <138222592+VitorBiliato10@users.noreply.github.com>
2024-08-09 07:44:48 -0300

    Add files via upload

---
+++ começar.js
@@ -0,0 +1,22 @@
+const cont1 = document.querySelector('.cont1')
+const cont2 = document.querySelector('.cont2')
+const cont3 = document.querySelector('.cont3')
+const cont4 = document.querySelector('.cont4')
+const cont5 = document.querySelector('.cont5')
+const cont6 = document.querySelector('.cont6')
+const h1 = document.querySelector('h1')
+const preto = document.querySelector('.preto')
+const button = document.querySelector('button')
+
+const startGame = () => {
+    cont1.style.display = 'none '
+    cont2.style.display = 'none '
+    cont3.style.display = 'none '
+    cont4.style.display = 'none '
+    cont5.style.display = 'none '
+    cont6.style.display = 'none '
+    h1.style.display = 'none '
+    preto.style.display = 'none '
+    button.style.display = 'none '
+
+}
